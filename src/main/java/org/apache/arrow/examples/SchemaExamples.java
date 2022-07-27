d62741fd7f88095fb0b88823da86b56d6509c407
Ashish <paliwalashish@gmail.com>
2022-07-26 20:57:32 -0700

    added fields example

---
+++ src/main/java/org/apache/arrow/examples/SchemaExamples.java
@@ -0,0 +1,35 @@
+package org.apache.arrow.examples;
+
+import org.apache.arrow.vector.types.pojo.ArrowType;
+import org.apache.arrow.vector.types.pojo.Field;
+import org.apache.arrow.vector.types.pojo.FieldType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SchemaExamples {
+
+    public void createFields() {
+        Field name = new Field("name", FieldType.nullable(new ArrowType.Utf8()), null);
+        System.out.println(name);
+
+        Field age = new Field("age", FieldType.nullable(new ArrowType.Int(32, true)), null);
+        System.out.println(age);
+
+        FieldType intType = new FieldType(true, new ArrowType.Int(32, true), null);
+        FieldType listType = new FieldType(true, new ArrowType.List(), null);
+        Field childField = new Field("intCol", intType, null);
+        List<Field> childFields = new ArrayList<>();
+        childFields.add(childField);
+        Field points = new Field("points", listType, childFields);
+
+        System.out.println(points);
+
+    }
+
+    public static void main(String[] args) {
+        SchemaExamples schemaExamples = new SchemaExamples();
+        schemaExamples.createFields();
+    }
+
+}
